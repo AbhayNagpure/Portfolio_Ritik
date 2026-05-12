4187679c4cb930a8cbbd198a7b0f9fbaefcf9e97
Abhay Nagpure <abhaynagpure99@gmail.com>
2026-05-12 21:27:35 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <main className="bg-[#0a0a0a] min-h-screen">
+      <p style={{ color: 'white' }}>Setup done</p>
+    </main>
+  )
+}
+
+export default App
